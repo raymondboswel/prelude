52c4c69bad6a146e863be2f2e0449182c77545f0
raymondboswel <raymondboswel@gmail.com>
2020-09-30 18:47:59 +0200

    Fixed migration issue, and added multiple cursors

---
+++ personal/multiple-cursors.el
@@ -0,0 +1,8 @@
+(prelude-require-packages '(multiple-cursors))
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
